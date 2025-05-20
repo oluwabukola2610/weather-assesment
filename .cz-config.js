ed46a73733b10120bc037decc02ca05ddcc37847
Your Name <roheemohmudashir@gmail.com>
2025-05-20 08:54:29 +0100

    feat(custom): build home screen and implement city search

---
+++ .cz-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  types: [
+    { value: "feat", name: "✨ feat: A new feature" },
+    { value: "fix", name: "🐞 fix: A bug fix" },
+    { value: "docs", name: "📚 docs: Documentation only changes" },
+  ],
+};
